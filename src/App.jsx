370b1b1c54ddf6da7c77bd6d6380789e07f90a1f
wolfsoul <alejandrof011021@gmail.com>
2023-02-19 01:17:13 +0100

    Se agrego boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <h1>Rick and Morty API</h1>
