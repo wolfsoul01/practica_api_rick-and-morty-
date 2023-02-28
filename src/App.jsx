aa5f18e32dc78459b3769f446b23b5233fda2db3
guilledev <guillermoprogcc@gmail.com>
2023-02-28 12:36:42 -0500

    Implementando Tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
-import Nav from './Componentes/Nav'
-import './App.css'
-import { useEffect,useState } from 'react'
+import Nav from "./Componentes/Nav";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const url = "https://rickandmortyapi.com/api/character";
 
-  const url="https://rickandmortyapi.com/api/character"
+  const [personaje, setPersonaje] = useState([]);
 
-  const [personaje,setPersonaje]=useState([]);
-
-  const PeticionUrl=(dato=>{
+  const peticionUrl = (dato) => {
     fetch(dato)
-    .then(respuesta=> respuesta.json())
-    .then(data => setPersonaje(data))
-    .catch(error=>console.error(error))})
+      .then((respuesta) => respuesta.json())
+      .then((json) =>
+        setPersonaje(
+          json.results.map(({name, id ,species,image}) => {
+            console.log(name);
+            return (
+              <div className="tarj" key={id}>
+                <h1>Personaje numero {id}</h1>
+                <img src={image} alt="Foto Personaje" />
+                <h4>Name:{name}</h4>
+                <p>Especie:{species}</p>
+              </div>
+            );
+          })
+        )
+      )
+      .catch(console.error);
+  };
 
-    useEffect(()=>{
-      PeticionUrl(url)
-    },[])
+  useEffect(() => {
+    peticionUrl(url);
+  }, []);
 
- 
-  
-  return (<>
-
-    <Nav/>
-
-    </>
-  )
+  return (
+    <div className="app">
+      <Nav />
+      <div className="tarjeta">{personaje}</div>
+    </div>
+  );
 }
 
-   export default App
+export default App;
--- src/Componentes/Nav.jsx
+++ src/Componentes/Nav.jsx
@@ -1,11 +1,13 @@
 function Nav() {
-    return (
-        <nav className="navbar navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Rick y Morty </a>
-        </div>
-      </nav>
-  )}
-  
-  export default Nav
-  
+  return (
+    <nav className="navbar navbar-dark bg-dark mg">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Rick y Morty
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
